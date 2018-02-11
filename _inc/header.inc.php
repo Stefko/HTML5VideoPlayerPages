87a19606213d06082399f80470781e4b6aa759db
Stefan Kohlewr <stefko@lobsterlounge.de>
2018-02-11 23:25:59 +0100

    Update header.inc.php

--- _inc/header.inc.php
+++ _inc/header.inc.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>HTML5 VideoPLayer Demo Pages</title>
+	<title>HTML5 VideoPlayer Demo Pages</title>
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 	<meta name="author" content="humans.txt">
